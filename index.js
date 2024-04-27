931fb1522cdca514517f1ba562e7200c3069f90d
26kkmm <dev26kkmm@gmail.com>
2024-04-27 11:57:59 +0200

    change datetime to show current

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function resizeTextarea() {
-  const textarea = document.getElementById("text");
-  textarea.style.height = "auto"; // Reset height to auto
-  textarea.style.height = textarea.scrollHeight + "px"; // Set height to fit content
-}
+const dayEl = document.querySelector(".day");
+const dateEl = document.querySelector(".date");
+
+let currentDate = new Date();
+let formattedDate = currentDate.toLocaleDateString("en-US", {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+});
+
+let dateComponents = formattedDate.split(", ");
+let day = dateComponents[0];
+let date = dateComponents[1] + " " + dateComponents[2];
+
+dateEl.textContent = date;
+dayEl.textContent = day;
+
+
